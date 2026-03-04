566190464b81dd1cc07739e5d900fa9620bf4d29
Xan-The-Programmer <146449874+Xan-The-Programmer@users.noreply.github.com>
2026-03-04 17:41:51 -0500

    Update Directory.py
    Added readto function

--- Directory.py
+++ Directory.py
@@ -1,4 +1,19 @@
+import os
+import csv
+
 head = "C:/Users/david/OneDrive - Osceola County School District/2025-2026 School Year/AP Research Evans Xan/"
 ana = head + "Analysis/"
 genf = ana + "General Files/"
 testcoms = genf +"Test Comments.csv"
+
+def readto(data = testcoms, debug = False):
+    with open(data, newline='', encoding='utf-8') as test:
+        content = csv.reader(test, delimiter= ',')
+        comments = [row[1] for row in content]
+
+        if debug:
+
+            print(comments)
+
+    return comments
+    
