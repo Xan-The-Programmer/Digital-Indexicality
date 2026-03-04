36c19a706055c7ddda2ef383e019ba36b12128e2
Xan-The-Programmer <146449874+Xan-The-Programmer@users.noreply.github.com>
2026-03-04 17:24:06 -0500

    Add files via upload

---
+++ Directory.py
@@ -0,0 +1,4 @@
+head = "C:/Users/david/OneDrive - Osceola County School District/2025-2026 School Year/AP Research Evans Xan/"
+ana = head + "Analysis/"
+genf = ana + "General Files/"
+testcoms = genf +"Test Comments.csv"
